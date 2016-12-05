61dc1f22ee53199b46709fa0dba290f6a5e6d91d
郝志宇 <zhi-yu.hao@utsoft.cn>
2016-12-05 09:03:38 +0000

    ...
    …

---
+++ myphp/test.php
@@ -0,0 +1,6 @@
+<?php
+	$first = $_REQUEST['first'];
+	$two = $_REQUEST['two'];
+	echo "first=".$first.'</br>';
+	echo "two=".$two;
+?>
---
+++ mysql/json.php
@@ -0,0 +1,42 @@
+<?php
+
+	header('Content-Type : text/html; charset = utf-8');
+	$retArr = array(
+		'succ' => '200', 
+		'data' => array(), 
+		'msg' => '请求成功', 
+	);
+	if (empty($_REQUEST['test_id'])) {
+		$retArr['succ'] = '404';
+		$retArr['msg'] = '请求失败';
+		exit(json_encode($retArr));
+	}
+	$test_id = $_REQUEST['test_id'];
+	$mysqli = new mysqli('localhost', 'root', 'qq641711559', 'test');
+	if ($mysqli->connect_errno) {
+    	echo "Connect failed: ", $mysqli->connect_error;
+    	exit;
+	}
+	if ($result = $mysqli->query("select * from test_table where test_id=$test_id")) {
+		$data = array();
+
+		if ($result->num_rows==1) {
+			foreach ($result->fetch_assoc() as $key => $value) {
+				$data[$key] = $value;
+			}
+			$retArr['data'] = $data;
+			echo (json_encode($retArr));
+		} elseif ($result->num_rows>1) {
+			foreach ($result as $key => $value) {
+				$data[$key] = $value;
+			}
+			$retArr['data'] = $data;
+			echo (json_encode($retArr));
+		} else {
+			echo (json_encode($retArr));
+		}
+				
+   		$result->close();
+	}
+	$mysqli->close();
+?>
